2030cc26e7e2f8762fb3aae7b6f2aeb41043166f
Vash Hsu <vash.hsu@gmail.com>
2016-09-07 15:27:56 +0800

    to combine duplicated encoding

---
+++ c2/8/Soundex.h
@@ -0,0 +1,90 @@
+#ifndef Soundex_h
+#define Soundex_h
+
+#include <string>
+#include <unordered_map>
+
+
+static const size_t MaxCodeLength{4};
+
+
+class Soundex
+{
+
+private:
+
+    std::string zeroPad(const std::string& word) const
+    {
+        int zerosNeeded = MaxCodeLength - word.length();
+        return word + std::string(zerosNeeded, '0');
+    }
+    
+    bool isComplete(const std::string& encoding) const
+    {
+        return encoding.length() == MaxCodeLength - 1;
+    }
+
+    std::string head(const std::string& word) const
+    {
+        return word.substr(0, 1);
+    }
+
+    std::string encodedDigits(const std::string& word) const
+    {
+        std::string encoding = "";
+        for (auto letter: word)
+        {
+            if (isComplete(encoding))
+            {
+                break;
+            }
+            // 1(1) -> 1
+            else if (encodedDigit(letter) != lastDigit(encoding))
+            {
+                encoding += encodedDigit(letter);
+            }
+        }
+        return encoding;
+    }
+    
+    std::string lastDigit(const std::string& encoding) const
+    {
+        if (encoding.empty())
+        {
+            return "";
+        }
+        // A reference to the last character in the string.
+        // If the string object is const-qualified, the function returns a const char&.
+        // Otherwise, it returns a char&.
+        return std::string(1, encoding.back());
+    }
+    
+    std::string tail(const std::string& word) const
+    {
+        return word.substr(1);
+    }
+
+public:
+
+    std::string encodedDigit(char letter) const
+    {
+        const std::unordered_map<char, std::string> encodings
+        {
+            {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
+            {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"}, 
+            {'d', "3"}, {'t', "3"},
+            {'l', "4"},
+            {'m', "5"}, {'n', "5"},
+            {'r', "6"}
+        };
+        auto result = encodings.find(letter);
+        return result == encodings.end() ? "": result->second;
+    }
+    
+    std::string encode(const std::string& word) const
+    {
+        return zeroPad( head(word) + encodedDigits(tail(word)));
+    }
+};
+
+#endif
---
+++ c2/8/SoundexTest.cpp
@@ -0,0 +1,76 @@
+#include <gmock/gmock.h>
+#include "Soundex.h"
+
+using namespace testing;
+
+
+class SoundexEncoding: public Test
+{
+
+public:
+    Soundex soundex;
+
+};
+
+
+TEST_F(SoundexEncoding, CombinesDuplicateEncodings)
+{
+    // {'b', "1"}, {'f', "1"}
+    // {'c', "2"}, {'g', "2"}
+    // {'d', "3"}, {'t', "3"},
+    ASSERT_EQ(soundex.encodedDigit('b'), soundex.encodedDigit('f'));
+    ASSERT_EQ(soundex.encodedDigit('c'), soundex.encodedDigit('g'));
+    ASSERT_EQ(soundex.encodedDigit('d'), soundex.encodedDigit('t'));
+    /*
+    Soundex.h:32:17: error: ¡¥std::__cxx11::string Soundex::encodedDigit(char) const¡¦ is private
+    std::string encodedDigit(char letter) const
+    */
+    ASSERT_EQ(soundex.encode("Abfcgdt"), "A123");
+}
+
+TEST_F(SoundexEncoding, IgnoresVowelLikeLetters)
+{    
+    ASSERT_EQ(soundex.encode("Baeiouhycdl"), "B234");
+}
+
+TEST_F(SoundexEncoding, LimitsLenghtToFourCharacters)
+{
+    ASSERT_EQ(soundex.encode("Dcdlb").length(), 4u);
+}
+
+TEST_F(SoundexEncoding, ReplaceMultipleConsonantsWithDigits)
+{
+    EXPECT_EQ(soundex.encode("Acdl"), "A234");
+}
+
+
+TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits)
+{
+    EXPECT_EQ(soundex.encode("Ab"), "A100");
+    EXPECT_EQ(soundex.encode("Ac"), "A200");
+    EXPECT_EQ(soundex.encode("Ad"), "A300");
+    EXPECT_EQ(soundex.encode("Ax"), "A200");
+}
+
+TEST_F(SoundexEncoding, IgnoresNonAlphabetics)
+{
+    ASSERT_EQ(soundex.encode("A#"), "A000");
+}
+
+TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
+{
+    std::string encoded = soundex.encode("A");
+    ASSERT_EQ(encoded, "A000");
+}
+
+TEST_F(SoundexEncoding,_PadsWithZeroZerosToEnsourceThreeDigits)
+{
+    std::string encoded = soundex.encode("I");
+    ASSERT_EQ(encoded, "I000");
+}
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
